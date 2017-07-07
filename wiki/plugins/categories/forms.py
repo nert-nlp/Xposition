693753260a5c95d333e40def6497fb6b853876b0
jcl240 <jcl240@georgetown.edu>
2017-07-07 12:26:14 -0400

    Fixed edit form error on start page. Was due to lack of checking if there were no associated categories to an article

--- wiki/plugins/categories/forms.py
+++ wiki/plugins/categories/forms.py
@@ -29,6 +29,7 @@ class SidebarForm(PluginSidebarFormMixin):
 
     ''' This edit form allows us to change what categories an article is in '''
     def __init__(self, article, request, *args, **kwargs):
+        self.validCategory = True
         self.article = article
         self.request = request
         super(SidebarForm, self).__init__(*args, **kwargs)
@@ -37,6 +38,8 @@ class SidebarForm(PluginSidebarFormMixin):
         self.fields['categories'].initial = article.categories.all
         self.fields['categories'].widget = forms.CheckboxSelectMultiple()
         self.fields['categories'].queryset = models.Category.objects.all()
+        if not self.article.categories.all():
+            self.validCategory = False
 
 
     def get_usermessage(self):
@@ -62,11 +65,17 @@ class EditCategoryForm(PluginSidebarFormMixin, forms.ModelForm):
         via the edit screen of its category landing article'''
 
     def __init__(self, article, request, *args, **kwargs):
+        self.validCategory = True
         self.article = article
         self.request = request
         super(EditCategoryForm, self).__init__(*args, **kwargs)
         self.fields['parent'].label_from_instance = lambda obj: mark_safe("%s" %  obj.parent.short_title + '--->' + obj.short_title if not obj.parent is None else obj.short_title)
-        self.fields['parent'].initial = self.article.categories.all()[0].parent
+        try:
+            self.fields['parent'].initial = self.article.categories.all()[0].parent
+        except:
+            if not self.article.categories.all():
+                self.validCategory = False
+            pass
 
         # logic to remove the category itself and its children from the available choices
         # to avoid edge cases
