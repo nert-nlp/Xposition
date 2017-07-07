220b402eddbcd17124047198c899901cdff8e119
jcl240 <jcl240@georgetown.edu>
2017-07-07 12:26:14 -0400

    Fixed edit form error on start page. Was due to lack of checking if there were no associated categories to an article

--- wiki/models/article.py
+++ wiki/models/article.py
@@ -37,7 +37,7 @@ class Article(models.Model):
 
     objects = managers.ArticleManager()
 
-    categories = models.ManyToManyField(Category, related_name="article_set")
+    categories = models.ManyToManyField(Category, related_name="article_set", blank=True)
 
     current_revision = models.OneToOneField(
         'ArticleRevision', verbose_name=_('current revision'),
