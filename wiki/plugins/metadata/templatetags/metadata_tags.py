0ff00b7fac3cf6f8736bc418d17376619e3a2044
jcl240 <jcl240@georgetown.edu>
2017-07-17 11:34:22 -0400

    Removed abstract from Metadata to provide abstraction

--- wiki/plugins/metadata/templatetags/metadata_tags.py
+++ wiki/plugins/metadata/templatetags/metadata_tags.py
@@ -6,6 +6,7 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def metadata_display(context, metadata):
+    metadata = metadata.metadatarevision.supersenserevision
     display = '<h2> Supersense: '+ metadata.name + '</h2>' + '<ul><li>Animacy: '+ str(metadata.animacy) +'</li>'
     if metadata.counterpart:
         display += '<li>Counterpart: <a href=/'+metadata.counterpart.current_revision.supersenserevision.name+'>' + metadata.counterpart.current_revision.supersenserevision.name +'</a></li></ul>'
