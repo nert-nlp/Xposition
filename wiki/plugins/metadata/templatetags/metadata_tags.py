367ade4928925fdf0d9f130da853ec045532df40
Nathan Schneider <nathan.schneider@georgetown.edu>
2017-08-07 15:14:25 -0700

    generalize metadata_display template tag to display whatever metadata fields are present

--- wiki/plugins/metadata/templatetags/metadata_tags.py
+++ wiki/plugins/metadata/templatetags/metadata_tags.py
@@ -1,15 +1,24 @@
 from django import template
 from django.utils.safestring import mark_safe
-
+from wiki.models import Article, ArticleRevision
+from wiki.models.pluginbase import RevisionPlugin, RevisionPluginRevision
+from wiki.plugins.metadata.models import MetadataRevision
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def metadata_display(context, metadata):
     metadata = metadata.metadatarevision.supersenserevision
-    display = '<h2> Supersense: '+ metadata.name + '</h2>' + '<ul><li>Animacy: '+ str(metadata.animacy) +'</li>'
-    if metadata.counterpart:
-        display += '<li>Counterpart: <a href=/'+metadata.counterpart.current_revision.supersenserevision.name+'>' + metadata.counterpart.current_revision.supersenserevision.name +'</a></li></ul>'
-    else:
-        display += '<li>This supersense does not have a counterpart.</li></ul>'
+    generic_flds = MetadataRevision._meta.get_fields()
+    display = '<h3 id="metadata">Metadata</h3>\n<table class="metadata">\n'
+    for fld in metadata._meta.get_fields(include_hidden=False):
+        if fld not in generic_flds and not fld.name.endswith('_ptr'):
+            display += f'    <tr><th style="padding: 10px;">{fld.name}</th><td>'
+            v = getattr(metadata, fld.name)
+            if hasattr(v, 'html'):
+                display += v.html()
+            else:
+                display += str(v)
+            display += '</td></tr>\n'
+    display += '</table>'
     return mark_safe(display)
