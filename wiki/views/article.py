c57e11c496c38a78fb58f2d92d062f61d9bdb55c
Nathan Schneider <nathan.schneider@georgetown.edu>
2017-08-19 18:03:15 -0400

    In displaying articles, account for SimpleMetadata as sister to Metadata; display language links in navbar

--- wiki/views/article.py
+++ wiki/views/article.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, unicode_literals
 import difflib
 import logging
 
-from wiki.plugins.metadata.models import Supersense, SupersenseRevision, Metadata
+from wiki.plugins.metadata.models import Supersense, SupersenseRevision, SimpleMetadata, Metadata
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
@@ -37,10 +37,18 @@ class ArticleView(ArticleMixin, TemplateView):
     @method_decorator(get_article(can_read=True))
     def dispatch(self, request, article, *args, **kwargs):
         try:
-            metadata = Metadata.objects.get(article = article)
-            self.template_name = metadata.current_revision.metadatarevision.template
-        except:
-            pass
+            metadata = SimpleMetadata.objects.get(article = article)
+        except SimpleMetadata.DoesNotExist:
+            try:
+                metadata = Metadata.objects.get(article = article)
+            except Metadata.DoesNotExist:
+                metadata = None
+
+        if metadata:
+            self.metadata = metadata
+            self.template_name = metadata.template
+            #metadata.current_revision.metadatarevision.template
+
         return super(
             ArticleView,
             self).dispatch(
@@ -53,7 +61,8 @@ class ArticleView(ArticleMixin, TemplateView):
         kwargs['selected_tab'] = 'view'
         try:
             #kwargs['metadata'] = self.article.current_revision.metadatarevision # TODO: I think this pointer is not being updated properly
-            kwargs['metadata'] = Metadata.objects.get(article = self.article).current_revision
+            if self.metadata:
+                kwargs['metadata'] = self.metadata
         except:
             pass
         return ArticleMixin.get_context_data(self, **kwargs)
