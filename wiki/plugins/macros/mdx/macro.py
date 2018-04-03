3369e95a158581bf28ea485f4b8a6ca5b09c0a16
ablodge <ajb341@georgetown.edu>
2018-04-02 20:54:17 -0400

    several fixes
    changed link to `<a href="..." class="...">...</a>` with classes: supersense, construal, adposition, usage. Added functionality for `[p en/in `$]` and `[p en/in Locus]` (short for Locus--Locus)

--- wiki/plugins/macros/mdx/macro.py
+++ wiki/plugins/macros/mdx/macro.py
@@ -1,255 +1,139 @@
 # -*- coding: utf-8 -*-
 
+
 from __future__ import absolute_import, unicode_literals
-
-
-
 import re
-
-
-
 import markdown
-
 from django.template.loader import render_to_string
-
 from django.utils.translation import ugettext as _
-
 from six import string_types
-
 from wiki.plugins.macros import settings
 
 
-
 # See:
-
 # http://stackoverflow.com/questions/430759/regex-for-managing-escaped-characters-for-items-like-string-literals
-
 re_sq_short = r"'([^'\\]*(?:\\.[^'\\]*)*)'"
 
-
 MACRO_RE = re.compile(
-
-    r".*(\[(?P<macro>\w+)(?P<args>(\s([\w/':-]+(%s)?))*)\]).*",
+    r".*(\[(?P<macro>\w+)(?P<args>(\s([\w/':`-]+(%s)?))*)\]).*",
     
     re.IGNORECASE | re.UNICODE)
 
 ARG_RE = re.compile(
-
-    r"\s(\w+:)?(?P<value>([\w/'-]+|%s))" %
-
+    r"\s(\w+:)?(?P<value>([\w/'`-]+|%s))" %
     re_sq_short,
-
     re.IGNORECASE | re.UNICODE)
 
-
-
-
-
-
 class MacroExtension(markdown.Extension):
-
-
-
     """ Macro plugin markdown extension for django-wiki. """
 
-
-
     def extendMarkdown(self, md, md_globals):
-
         """ Insert MacroPreprocessor before ReferencePreprocessor. """
-
         md.preprocessors.add('dw-macros', MacroPreprocessor(md), '>html_block')
 
-
-
-
-
-class MacroPreprocessor(markdown.preprocessors.Preprocessor):
-
-
-
+		class MacroPreprocessor(markdown.preprocessors.Preprocessor):
     """django-wiki macro preprocessor - parse text for various [some_macro] and
-
     [some_macro (kw:arg)*] references. """
 
-
-
     def run(self, lines):
-
         # Look at all those indentations.
-
         # That's insane, let's get a helper library
-
         # Please note that this pattern is also in plugins.images
-
         new_text = []
-
         for line in lines:
-
             m = MACRO_RE.match(line)
-
             if m:
-
                 macro = m.group('macro').strip()
-
                 if macro in settings.METHODS and hasattr(self, macro):
-
                     args = m.group('args')
-
                     if args:
-
                         args_list = []
-
                         for arg in ARG_RE.finditer(args):
-
                             value = arg.group('value')
-
                             #if value is None:
-
                                 #value = True
-
                             if isinstance(value, string_types):
-
                                 # If value is enclosed with ': Remove and
-
                                 # remove escape sequences
-
                                 if value.startswith("'") and len(value) > 2:
-
                                     value = value[1:-1]
-
                                     value = value.replace("\\\\", "¤KEEPME¤")
-
                                     value = value.replace("\\", "")
-
                                     value = value.replace("¤KEEPME¤", "\\")
-
                             if value is not None:
-
                                 args_list.append(str(len(args_list)))
-
                                 args_list.append(value)
-
                         line = getattr(self, macro)(*args_list)
-
                     else:
-
                         line = getattr(self, macro)()
-
             if line is not None:
-
                 new_text.append(line)
-
         return new_text
 
-
-
     def article_list(self, depth="2"):
-
         html = render_to_string(
-
             "wiki/plugins/macros/article_list.html",
-
             context={
-
                 'article_children': self.markdown.article.get_children(
-
                     article__current_revision__deleted=False),
-
                 'depth': int(depth) + 1,
-
             })
-
         return self.markdown.htmlStash.store(html, safe=True)
-
     article_list.meta = dict(
-
         short_description=_('Article list'),
-
         help_text=_('Insert a list of articles in this level.'),
-
         example_code='[article_list depth:2]',
-
         args={'depth': _('Maximum depth to show levels for.')}
-
     )
-
-
-
+	
     def toc(self):
-
         return "[TOC]"
-
     toc.meta = dict(
-
         short_description=_('Table of contents'),
-
         help_text=_('Insert a table of contents matching the headings.'),
-
         example_code='[TOC]',
-
         args={}
-
     )
-
-
-
-    def wikilink(self):
-
+    
+	def wikilink(self):
         return ""
-
     wikilink.meta = dict(
-
         short_description=_('WikiLinks'),
-
         help_text=_(
-
             'Insert a link to another wiki page with a short notation.'),
-
         example_code='[[WikiLink]]',
-
         args={})
-
     
-
+	def link(text, link, clazz):
+		return '<a href="'link+'" class="'+clazz+'">'+text+'</a>'
+	
     def p(self, *args):
-
         if len(args)>1:
-
             prep, construal = args[0], args[1]
-
-            return '['+prep+'](/'+prep+'/'+construal+')'
-
-        return '['+args[0]+'](/'+args[0]+')'
-
+			if '`' in args[0]:
+				return link(prep, '/'+prep+'/'+construal.replace('`', "'"), 'usage')
+			elif '--' in args[0]:
+				return link(prep, '/'+prep+'/'+construal, 'usage')
+			else:
+				return link(prep, '/'+prep+'/'+construal+'--'+construal, 'usage')
+        return link(prep, '/'+prep, 'adposition')
     p.meta = dict(
-
-        short_description=_('Link to English Adposition, Usage'),
-
+        short_description=_('Link to Adposition, Usage'),
         help_text=_('Create a link to a preposition or preposition-construal pair'),
-
         example_code='[p en/in] or [p en/in Locus--Locus]',
-
         args={'prep': _('Name of adposition'), 'construal': _('Name of construal')}
-
     )
-
-
-
     
-
     def ss(self, *args):
-
-        return '['+args[0]+'](/'+args[0]+')'
-
+		if '`' in args[0]:
+			return link(args[0], '/'+args[0].replace('`', "'"), 'construal')
+		elif '--' in args[0]:
+			return link(args[0], '/'+args[0], 'construal')
+		else:
+			return link(args[0], '/'+args[0], 'supersense')
     ss.meta = dict(
-
         short_description=_('Link to Supersense or Construal'),
-
         help_text=_('Create a link to a supersense or construal'),
-
         example_code='[ss Locus] or [ss Locus--Locus]',
-
         args={'name': _('Name of supersense/construal label')}
-
     )
