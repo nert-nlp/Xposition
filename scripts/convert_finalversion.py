af1b743264741f92feb7a59b9d2fefdfa50699d4
ablodge <ajb341@georgetown.edu>
2018-06-13 17:46:13 -0400

    Add Latex conversion scripts

---
+++ scripts/convert_finalversion.py
@@ -0,0 +1,91 @@
+import re, os
+
+os.chdir(r'C:\Users\Austin\Desktop\Xposition\misc')
+
+dir = 'markdown'
+dir2 = 'markdown-final'
+
+P_RE = re.compile(r'\[([\w\\\'-])+\]\(/en/(?P<p>[\w\'\\-]+)\)')
+SS_RE = re.compile(r'\[[\w$`-]+\]\(/(?P<ss>[\w$`-]+)\)')
+NEW_SS_RE = re.compile('(- )?\[[\w$`-]+\]\(/(?P<ss>[\w$`-]+)\)')
+ALT_SE_RE = re.compile('\(\[[\w$`-]+\]\(/(?P<ss>[\w$`-]+)\)\)')
+ORDINARY_RE = re.compile('^\s*[^-\s].*')
+EXAMPLE_RE = re.compile('- (<a id="ex(?P<id>\w+)"></a>)?"<i>(?P<ex>.+)</i>"')
+EX_REF_RE = re.compile('\[[\w .,-]+?\]\(/(?P<ss>[\w$`-]+)/#ex(?P<id>\w+)\)')
+
+ids = {}
+
+if not os.path.exists(dir2):
+    os.makedirs(dir2)
+
+for file in os.listdir(dir):
+    if file.endswith('.txt'):
+        with open(os.path.join(dir,file), 'r', encoding='utf8') as f:
+            new_text = []
+
+
+            default_ss = file.replace('.txt', '')
+            tmp_ss = None
+            id_index = 1
+
+            print(default_ss)
+            for line in f:
+                # fix Part/Portion
+                line = line.replace('Part/Portion', 'PartPortion')
+
+                if NEW_SS_RE.match(line):
+                    default_ss = NEW_SS_RE.match(line).group('ss')
+                elif ORDINARY_RE.match(line):
+                    default_ss = file.replace('.txt', '')
+                elif ALT_SE_RE.search(line):
+                    tmp_ss = ALT_SE_RE.search(line).group('ss')
+                elif '**'+file.replace('.txt', '')+'**' in line:
+                    tmp_ss = file.replace('.txt', '')
+                # convert p
+                for p_link in P_RE.finditer(line):
+                    prep = p_link.group('p')
+                    if ORDINARY_RE.match(line):
+                        line = line.replace(p_link.group(0), '[p en/' + prep + ']')
+                    else:
+                        line = line.replace(p_link.group(0), '[p en/'+prep+' '+(tmp_ss if tmp_ss else default_ss)+']')
+                # convert ss
+                for ss_link in SS_RE.finditer(line):
+                    ss = ss_link.group('ss')
+                    line = line.replace(ss_link.group(0), '[ss '+ss+']')
+                # convert examples
+                for example in EXAMPLE_RE.finditer(line):
+                    ex = example.group('ex').replace('\'','\\\'')
+                    ex = ex.replace('"','')
+                    ex = ex.replace('<i>','').replace('</i>','')
+                    ex = ex.replace('</i>','')
+                    id = example.group('id')
+                    if not id:
+                        id = str(id_index)+'?'
+                    ids[id] = id_index
+                    line = line.replace(example.group(0), '- [ex '+'{0:03d}'.format(id_index)+' '+'\''+ex+'\''+']')
+                    id_index += 1
+
+                tmp_ss = None
+                new_text.append(line)
+
+            with open(os.path.join(dir2, file), 'w+', encoding='utf8') as f2:
+                f2.write(''.join(new_text))
+
+for file in os.listdir(dir2):
+    if file.endswith('.txt'):
+        with open(os.path.join(dir2, file), 'r', encoding='utf8') as f:
+            new_text = []
+            for i, line in enumerate(f):
+                # convert example refs
+                for ex_link in EX_REF_RE.finditer(line):
+                    ss = ex_link.group('ss')
+                    id = ex_link.group('id')
+                    if id in ids:
+                        line = line.replace(ex_link.group(0), '[exref ' + '{0:03d}'.format(ids[id]) + ' ' + ss + ']')
+                    else:
+                        line = line.replace(ex_link.group(0), '[exref ' + '???' + ' ' + ss + ']')
+                        print('fix exref', file, id, ss)
+                new_text.append(line)
+        with open(os.path.join(dir2, file), 'w', encoding='utf8') as f2:
+            f2.write(''.join(new_text))
+
---
+++ scripts/convert_tex2markdown.py
@@ -0,0 +1,250 @@
+"""
+This code is for importing our Preposition Supersense v2 guidelines
+into our new Xposition website/wiki
+3/21/18 Austin Blodgett
+"""
+import re, os
+
+os.chdir(r'C:\Users\Austin\Desktop\Xposition\misc')
+
+dir = 'markdown'
+
+example_index = 1
+
+def replace_circumfix(s, prefix, repl_prefix, repl_suffix):
+    m = re.search(prefix,s)
+    start_index = m.end()
+    contents = ''
+    i=0
+    count = 1
+    while True:
+       c = s[start_index+i]
+       if c=='{':
+           count += 1
+       elif c=='}':
+           count -= 1
+       if count == 0:
+           break
+       contents += c
+       i+=1
+    s1 = s[:m.start()]
+    s2 = repl_prefix + contents + repl_suffix
+    s3 = s[start_index+i+1:]
+    return s1 + s2 + s3
+
+def replace_circumfixes(s, prefix, repl_prefix, repl_suffix):
+    while re.search(prefix,s):
+        s = replace_circumfix(s, prefix, repl_prefix, repl_suffix)
+    return s
+
+def handle_ex(s):
+    global example_index
+    s = re.sub(r",(\s)(\s)+", ", ", s, re.MULTILINE)
+    s = re.sub(r"(?<=[a-z])(\s+)(?=[a-z])", " ", s, re.MULTILINE)
+    s = re.sub(r"(?<=\S)([ \t]*)\\ex", "\n\\ex", s)
+    s = re.sub(r'\\ex(?=\\)', r'\ex ', s)
+    s = re.sub(r'\\ex\t', '\ex ', s)
+    s = re.sub(r'\\sn(?=\\)', r'\sn ', s)
+    s = re.sub(r'\\sn\t', '\sn ', s)
+
+
+    match = re.compile(r'\\ex[ {]')
+    while match.search(s):
+        m = match.search(s)
+        ex_bookmark = '<a id="ex' + '{0:04d}'.format(example_index) + '"></a>'
+        if s[m.end()-1]=='{':
+            s = replace_circumfix(s, r'\\ex{', '- ' + ex_bookmark + '"<i>', '</i>"')
+            example_index += 1
+        else:
+            content = '- ' + ex_bookmark + '"<i>' + s[m.end():s.index('\n',m.end())+1].strip() + '</i>"'
+            # if '\label' in content:
+            #     label = re.search(r'\\label{(.*?)}', content).group(1)
+            #     print(example_index,label)
+            s = s[:m.start()]+content+'\n'+s[s.index('\n',m.end())+1:]
+            example_index += 1
+
+    match = re.compile(r'\\sn[ {]')
+    while match.search(s):
+        m = match.search(s)
+        if s[m.end() - 1] == '{':
+            s = replace_circumfix(s, r'\\sn{', '- "<i>', '</i>"')
+            example_index += 1
+        else:
+            content = '- "<i>' + s[m.end():s.index('\n',m.end())+1].strip() + '</i>"'
+            s = s[:m.start()] + content + '\n' + s[s.index('\n', m.end()) + 1:]
+            example_index += 1
+
+    # adjust indentation
+    if '1. ' in s:
+        s = re.sub(r"\n( *)- <a", "\n\n    - <a", s)
+        s = re.sub(r':(\n\s*)(\n\s*)+- <a', ':\n\n    - <a', s)
+    else:
+        s = re.sub(r"\n( *)- <a", "\n\n- <a", s)
+        s = re.sub(r':(\n\s*)(\n\s*)+- <a', ':\n\n- <a', s)
+
+    return s
+
+
+def convert(ifile, ofile):
+    footnotes = []
+
+    with open(ifile, 'r', encoding='utf8') as f:
+        f = f.readlines()
+        #title = re.search(r'\\hier[ABCDE]def{(.*?)}', f[0]).group(1)
+        title = str(ifile)
+
+        # # handle junk line by line
+        for i, line in enumerate(f):
+            if line.strip().startswith('%'):
+                 f[i] = ''
+            if '\multicolsep' in f[i]:
+                f[i] = ''
+            if '\label' in f[i]:
+                f[i] = re.sub(r'\\label{(.*?)}', '', f[i])
+            if '\\noindent' in f[i]:
+                f[i] = re.sub(r'\\noindent', '', f[i])
+            if r'{xlist}' in f[i]:
+                f[i] = re.sub(r'\\begin{xlist}', '', f[i])
+                f[i] = re.sub(r'\\end{xlist}', '', f[i])
+                if re.search(r'\\rf{(.*?)}{(.*?)}', f[i]):
+                    print(ifile, re.search(r'\\rf{(.*?)}{(.*?)}', f[i]).group(0))
+                    print('\n'.join(f[i+1:i+4]))
+                if re.search(r'\\psst{(.*?)}', f[i]):
+                    print(ifile, re.search(r'\\psst{(.*?)}', f[i]).group(0))
+                    print('\n'.join(f[i + 1:i + 4]))
+            if ' %' in f[i]:
+                f[i] = f[i][0:f[i].index(' %')]
+            if f[i].strip() == '\ex':
+                f[i] = ''
+            # take care of [] brackets
+            f[i] = re.sub(r'[ \[]\[', r'\[', f[i])
+            f[i] = re.sub(r'\][\s\]]', r'\]', f[i])
+            # junk whitespace
+            f[i] = re.sub(r'\r', r'', f[i])
+            f[i] = f[i].strip()+'\n'
+
+        data = ''.join(f[1:])
+
+        # handle shortdef
+        data = replace_circumfixes(data, r'\\shortdef{', r'|', '|')
+
+        # embold title of article
+        data = re.sub(r'\\psst{' + title + '}', r'**' + title + '**', data)
+
+        # handle \p{}
+        data = replace_circumfixes(data, r'\\p{', ' [[en/', ']] ')
+        # handle \p*{}{}
+        data = replace_circumfixes(data, r'\\p\*{', '\\p1{', '](/en/\\p2')
+        data = replace_circumfixes(data, r'\\p2{', '', '}')
+        data = replace_circumfixes(data, r'\\p1{', ' [', ') ')
+        # handle \psst{}
+        data = replace_circumfixes(data, r'\\psst{', ' [[', ']] ')
+        # handle \rf{}{}
+        data = replace_circumfixes(data, r'\\rf{', '\\rf1{', '--\\rf2')
+        data = replace_circumfixes(data, r'\\rf2{', '', '}')
+        data = replace_circumfixes(data, r'\\rf1{', ' [[', ']] ')
+        # reformat links
+        while re.search(r' \[\[(.*?)\]\] ', data):
+            ref = re.search(r' \[\[(.*?)\]\] ', data).group(1)
+            data = data.replace(' [[' + ref + ']] ', '[' + ref + '](/' + ref + ')')
+        data = re.sub(r'\[en/', '[', data)
+
+        # add numbered list
+        i = 1
+        while re.search(r'\\item',  data):
+            data = re.sub(r'\\item',  str(i)+'.', data, count=1)
+            i += 1
+        # handle paragraph
+        data = replace_circumfixes(data, r'\\paragraph{', '- **', '**')
+
+        # handle bold, italics, quotes
+        data = replace_circumfixes(data, r'\\textbf{', '**', '**')
+        data = replace_circumfixes(data, r'\\textit{', '<i>', '</i>')
+        data = replace_circumfixes(data, r'\\w{', '<i>', '</i>')
+        data = replace_circumfixes(data, r'\\emph{', '<i>', '</i>')
+        data = replace_circumfixes(data, r'\\uline{', '<u>', '</u>')
+        data = replace_circumfixes(data, r'\\texttt{', '`', '`')
+        data = re.sub(r'``',"\"", data)
+        data = re.sub(r'`', "'", data)
+        data = re.sub(r"''", "\"", data)
+        data = re.sub(r"---", "—", data)
+        data = re.sub(r"[.]\\ ", ".", data)
+
+        # math
+        data = re.sub(r"~~", " ", data)
+        data = re.sub(r"~", " ", data)
+        data = re.sub(r"\\slash ", r"/", data)
+        data = re.sub(r"(?<=[A-Za-z])\\_(?=[A-Za-z])", "-", data)
+        data = re.sub(r"\\dots", "...", data)
+        data = re.sub(r"\$\\rightarrow\$", "→", data)
+        data = re.sub(r"\$\\nrightarrow\$", "↛", data)
+        data = re.sub(r"\$_{\\text{\\backposs}}\$", "<sub>[[`$]]</sub>", data)
+        data = re.sub(r"\$_{\\text{", '<sub>', data)
+        data = re.sub(r"}}\$", '</sub>', data)
+        data = re.sub(r"\$_{", '<sub>', data)
+        data = re.sub(r"}\$", '</sub>', data)
+
+        # misc labels
+        data = replace_circumfixes(data, r'\\sst{', '<i>', '</i>')
+        data = replace_circumfixes(data, r'\\lbl{', '<i>', '</i>')
+        data = replace_circumfixes(data, r'\\pex{', '<i>', '</i>')
+        data = replace_circumfixes(data, r'\\choices{', r'<u>', r'</u>')
+        data = re.sub(r"\\\\", "</u>/<u>", data)
+        data = replace_circumfixes(data, r'\\url{', '', '')
+        data = re.sub(r'\\psstX{Part/Portion}{Part/Portion}', " [[Part/Portion]] ", data)
+        data = re.sub(r"\\end{history}", "}", data)
+        data = replace_circumfixes(data, r'\\begin{history}', '\n<!-- ', ' -->')
+        data = replace_circumfixes(data, r'\\futureversion{', '\n<!-- ', ' -->')
+        data = re.sub(r"\\(end|begin){(.*?)}", "", data)
+
+
+        # handle footnotes, citations, references
+        data = replace_circumfixes(data, r'\\footnote{', '<footnote1>', '</footnote1>')
+        data = re.sub(r'\\citep\[', '\citep{', data)
+        data = re.sub(r'\\citet\[', '\citet{', data)
+        data = re.sub(r'\]{', ', ', data)
+        data = replace_circumfixes(data, r'\\citep{', '<footnote2>(', ')</footnote2>')
+        data = replace_circumfixes(data, r'\\citet{', '<footnote2>(', ')</footnote2>')
+        data = replace_circumfixes(data, r'\\cref{', '<footnote2>', '</footnote2>')
+        data = replace_circumfixes(data, r'\\ref{', '<footnote2>', '</footnote2>')
+        data = replace_circumfixes(data, r'\\exp{', '<footnote2>', '</footnote2>')
+        data = replace_circumfixes(data, r'\\Cref{', '<footnote2>', '</footnote2>')
+        j = 1
+        for i in [1, 2]:
+            m = re.compile('<footnote'+str(i)+'>(.*?)</footnote'+str(i)+'>', re.MULTILINE|re.DOTALL)
+            while m.search(data):
+                s = m.search(data).group(1)
+                s = re.sub('<(/)?footnote[12]>', '', s)
+                footnotes.append(s)
+                data = m.sub('[^' + str(j) + ']', data, count=1)
+                j += 1
+        # add footnotes
+        k = 1
+        for foot in footnotes:
+            data += '\n[^'+str(k)+']: '+foot
+            k += 1
+
+        # handle examples
+        data = handle_ex(data)
+
+        # whitespace
+        data = re.sub(r"\n\s+\n", "\n\n", data)
+        data = re.sub(r"\n\n+\n", "\n\n", data)
+        # data = re.sub(r'\\label{(.*?)}', '', data)
+
+    with open(ofile, 'w+', encoding='utf8') as f:
+        # i=0
+        # while re.search(r'\\[A-Za-z]+', data[i:]):
+        #     m = re.search(r'\\[A-Za-z]+', data[i:])
+        #     print(ifile, m.group(0))
+        #     i = m.end()
+        f.write(data)
+
+
+
+for file in os.listdir('.'):
+    # print(file)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    if file.endswith('.tex'):
+        convert(file,os.path.join(dir,file.replace('.tex','.txt')))
