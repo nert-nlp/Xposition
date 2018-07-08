75a0d202aee5cacc832d41632f13e08baf96e1f2
ablodge <ajb341@georgetown.edu>
2018-07-08 12:27:57 -0400

    Add import for construal articles

--- scripts/articles_for_import.py
+++ scripts/articles_for_import.py
@@ -5,25 +5,31 @@ from collections import defaultdict
 from wiki.plugins.metadata import models as ms
 
 dir = 'markdown-and-macros'
+dir2 = 'markdown-construals'
 
-ids = defaultdict(int)
-ids.setdefault(0)
-for a in ms.Article.objects.all():
-    ids[str(a.urlpath_set.all()[0])[:-1]] = a.pk
-# print(ids)
 
-articles = []
+def write_json(dir, output):
+    ids = defaultdict(int)
+    ids.setdefault(0)
+    for a in ms.Article.objects.all():
+        ids[str(a.urlpath_set.all()[0])[:-1]] = a.pk
+    # print(ids)
 
-for file in os.listdir(dir):
-    if file.endswith('.txt'):
-        content = open(os.path.join(dir, file), 'r', encoding='utf8').read()
-        content = content.split('|')[-1]
+    articles = []
 
-        articles.append(
-            {'content':content,
-             'title':file.replace('.txt',''),
-             'article_id':str(ids[file.replace('.txt','')])}
-        )
+    for file in os.listdir(dir):
+        if file.endswith('.txt'):
+            content = open(os.path.join(dir, file), 'r', encoding='utf8').read()
+            content = content.split('|')[-1]
 
-with open('supersense_article_revisions.json', 'w', encoding='utf8') as f:
-    json.dump(articles,f)
+            articles.append(
+                {'content':content,
+                 'title':file.replace('.txt',''),
+                 'article_id':str(ids[file.replace('.txt','')])}
+            )
+
+    with open(output, 'w', encoding='utf8') as f:
+        json.dump(articles,f)
+
+write_json(dir,'supersense_article_revisions.json')
+write_json(dir2, 'construal_article_revisions.json')
