42e7ec29606c9b01cc8f9544ae2b572e75c7a6f3
ablodge <ajb341@georgetown.edu>
2018-07-19 22:20:21 -0400

    #100 slight fix

--- scripts/split_latex.py
+++ scripts/split_latex.py
@@ -1,19 +1,20 @@
 import os, re
 
-os.chdir(r'C:\Users\Austin\Desktop')
-
-file = r'psst2.tex'
-dir = r'Xposition\local\Xposition\scripts\tex'
+file = 'latex.tex'
+dir = 'tex-test'
 
 with open(file, 'r', encoding='utf8') as f:
     data = ''.join(f.readlines())
-    split = re.split(r'(\\hier[ABCDE]def{|\\(sub)?section{)', data)
+    split = re.split(r'(\\hier[ABCDE]def{|\\section{|\\subsection{)', data)
+    print(len(split))
     for s in split:
-        title = s[:s.find("}")]
+        title = s[:s.find("}")].replace('?','').replace('/','')
+        title = title.replace('\\backi', '`i').replace('\\backd', '`d')
+        title = title.replace('\\backc', '`c').replace('\\backposs', '`$')
         if '\\' in title:
             continue
         print(title)
-        if not os.path.exists('tex'):
-            os.mkdir('tex')
+        if not os.path.exists(dir):
+            os.mkdir(dir)
         with open(os.path.join(dir,title+'.tex'), 'w+', encoding='utf8') as f2:
             f2.write('\\section{'+s)
