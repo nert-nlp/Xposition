3bd3dc92336a192cf3f318bde6c886eaa99a8fcb
ablodge <ajb341@georgetown.edu>
2018-08-12 17:02:27 -0400

    #45 base.py fix
    xp/base.py contains everything that testproject/base.py does. Extra code goes at the bottom.

--- xposition/xp/settings/base.py
+++ xposition/xp/settings/base.py
@@ -27,6 +27,165 @@ ALLOWED_HOSTS = ['*']
 
 
 
+INSTALLED_APPS = [
+    'django.contrib.humanize.apps.HumanizeConfig',
+    'django.contrib.auth.apps.AuthConfig',
+    'django.contrib.contenttypes.apps.ContentTypesConfig',
+    'django.contrib.sessions.apps.SessionsConfig',
+    'django.contrib.sites.apps.SitesConfig',
+    'django.contrib.messages.apps.MessagesConfig',
+    'django.contrib.staticfiles.apps.StaticFilesConfig',
+    'django.contrib.admin.apps.AdminConfig',
+    'django.contrib.admindocs.apps.AdminDocsConfig',
+    'sekizai',
+    'sorl.thumbnail',
+    "django_nyt.apps.DjangoNytConfig",
+    "wiki.apps.WikiConfig",
+    "wiki.plugins.macros.apps.MacrosConfig",
+    'wiki.plugins.help.apps.HelpConfig',
+    'wiki.plugins.links.apps.LinksConfig',
+    "wiki.plugins.images.apps.ImagesConfig",
+    "wiki.plugins.attachments.apps.AttachmentsConfig",
+    "wiki.plugins.notifications.apps.NotificationsConfig",
+    'wiki.plugins.globalhistory.apps.GlobalHistoryConfig',
+    'mptt',
+    'wiki.plugins.categories',
+    'wiki.plugins.categories.editor',
+    'wiki.plugins.metadata',
+    'import_export',
+    # 'silk',
+]
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+]
+
+ROOT_URLCONF = 'testproject.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(PROJECT_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.request",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+                "sekizai.context_processors.sekizai",
+            ],
+            'debug': DEBUG,
+        },
+    },
+]
+
+WSGI_APPLICATION = 'testproject.wsgi.application'
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('wiki:get', kwargs={'path': ''})
+
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, 'db', 'prepopulated.db'),
+    }
+}
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+#         'LOCATION': 'article_cache'
+#     }
+# }
+
+# Password validation
+# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
+
+TIME_ZONE = 'America/New_York'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-US'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+WIKI_ANONYMOUS_WRITE = True
+WIKI_ANONYMOUS_CREATE = False
+
+WIKI_URL_CASE_SENSITIVE = True
+WIKI_URL_CONFIG_CLASS = 'wiki.plugins.metadata.urls.XpositionURLPatterns'
+
+WIKI_MARKDOWN_HTML_WHITELIST = ['sub', 'sup', 'hr', 'u', 'br']
+
+# To make deployment happy
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = False   # setting to True breaks django-wiki login
+CSRF_COOKIE_SECURE = False  # setting to True breaks django-wiki login
+X_FRAME_OPTIONS = 'DENY'
+
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel'
+]
+
+
 INSTALLED_APPS += [
     'django_nyt',
     'django_tables2',
